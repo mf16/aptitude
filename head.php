8c5502ba97213484628f849addfa3aa236b0fa23
Tyler Slater <tslater@goaptitude.com>
2014-03-14 17:00:28 -0600

    Creation of includes/js

--- head.php
+++ head.php
@@ -2,9 +2,9 @@
 ?>
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title></title>
-<meta name="description" content="">
-<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Aptitude | Empowering the learner</title>
+<meta name="description" content="Aptitude learning">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="css/style.css">
 <?php
 ?>
