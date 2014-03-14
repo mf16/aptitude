2f17d3699df782da6c194990f91e9d73fe42e1f4
root <mitchfacer@gmail.com>
2014-03-14 00:02:14 -0600

    Changed home page class including and instantiating to use URL path

--- template.php
+++ template.php
@@ -10,13 +10,13 @@
 
 include_once "includes/global.php";
 
-$path = ltrim(substr($_SERVER['REQUEST_URI'],0), '/');    // Trim leading slash(es)
-$elements = explode('/', $path);                // Split path on slashes
+$path = ltrim(substr($_SERVER['REQUEST_URI'],0), '/');
+$elements = explode('/', $path);
 
 if(count($elements) == 0 || (count($elements)==2 && !$elements[1])){                       
 	echo 'homepage';
 	//$drawObj = new homepage();
-} else switch (strtolower($elements[1])){
+}/* else switch (strtolower($elements[1])){
 	case 'signup':
 		include 'includes/class.Signup.php';
 		// include js? js folder or just class.Signup.js?
@@ -26,6 +26,18 @@ if(count($elements) == 0 || (count($elements)==2 && !$elements[1])){
 		echo '404 error';die();
 		//Show404Error();
 }
+*/
+$className = '';
+array_shift($elements); // using localhost/aptitude
+foreach($elements as $key => $element){
+	$className .= ucwords($element);
+	if($key<sizeof($elements)-1){
+		$className .= '_';
+	}
+}
+
+include 'includes/class.'.$className.'.php'; //includes class based on URL path
+$drawObj = new $className;
 
 if(isset($_SESSION['logged'])){
 
@@ -41,8 +53,8 @@ if(isset($_SESSION['logged'])){
 ?>
 </head>
 <body>
-<?php
- $drawObj->draw(); //??
-?>
+	<?php
+	$drawObj->draw(); //??
+	?>
 </body>
 </html>
