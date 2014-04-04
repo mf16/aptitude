36b8bba122867a1aaf1df75157122788909ff49e
Mitch <mitchfacer@gmail.com>
2014-04-04 09:35:42 -0600

    Added backend for class roster

--- includes/class.GroupDAO.php
+++ includes/class.GroupDAO.php
@@ -5,6 +5,12 @@ class GroupDAO{
 	function __construct(){
 	}
 
+	function getStudentsByClassid($classid){
+		global $db;
+		$query='SELECT user_firstname,user_lastname,email FROM universal.users u,math.group_members g WHERE g.group_id=? ORDER BY user_lastname ASC';
+		$results=query($query,$classid);
+		return $results;
+	}
 	function getGroupByAdminid($subject,$userid){
 		global $db;
 		$results=query('SELECT group_id,group_name FROM math.groups WHERE group_admin_id=? ORDER BY end_date DESC;',$_SESSION['userid']);
