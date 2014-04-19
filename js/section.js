14feb453b806a780650ce310a813b348414d64c1
Tyler Slater <tslater@goaptitude.com>
2014-04-19 17:27:52 -0600

    Section.js mod

--- js/section.js
+++ js/section.js
@@ -10,6 +10,7 @@ function changeModal(title, content){
 
 
 JXG.Options.grid.snapToGrid = true;
+JXG.Options.elements.fixed = true;
 var brd = JXG.JSXGraph.initBoard('jxgbox', {boundingbox:[-6,2,6,-2]});
 var axisx = brd.create('axis', [[0,0], [1,0]], 
   {
@@ -23,5 +24,6 @@ var axisx = brd.create('axis', [[0,0], [1,0]],
       minorTicks: 0
     }
   });
-var p = brd.create('glider', [2, 0, axisx], {snapWidth:1});
-var p = brd.create('glider', [4, 0, axisx], {snapWidth:1});
+var p1 = brd.create('glider', [2, 0, axisx], {snapWidth:1});
+var p2 = brd.create('glider', [4, 0, axisx], {snapWidth:1});
+var l1 = brd.create('segment', [p1, p2]);
