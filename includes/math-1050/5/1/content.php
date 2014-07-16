d960538e8f6eef87f22d8b32aeb2a85014b3546f
Tyler Slater <tslater@goaptitude.com>
2014-07-16 09:00:55 -0600

    Graph pretest  problem styling

--- includes/math-1050/5/1/content.php
+++ includes/math-1050/5/1/content.php
@@ -1,5 +1,5 @@
-<link rel="stylesheet" type="text/css" href="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>css/includes/math-1050/5/1/pretest_page_specific_styling.css">
 <link rel="stylesheet" type="text/css" href="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>css/includes/math-1050/5/1/pretest_global_styling.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>css/includes/math-1050/5/1/pretest_page_specific_styling.css">
 <div id="workArea"></div>
 <!-- wrapper -->
 <div class="page-wrap pretestWrapper">
@@ -8,32 +8,35 @@
 		</section>
 		<section class="body">
 			<section class="row-fluid">
-				<section class="col-xs-12 text-body">
-					<section class="col-sm-2 col-xs-2 no-padding">
-						<div class="chapter-number">
-							<img src="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>img/global/chapter-arrow.png"/>
-						</div>
-					</section>
-					<section class="col-md-10 material-body col-sm-offset-1 col-xs-offset-1 col-md-offset-0">
-						<section class="row-fluid section-title-container">
-							<h1 class="section-title">sets of numbers</h1>
-							<span class="section-number">PREQUIZ</span><br>
-						</section>
-						<section class="row-fluid prequizExplanation">
-							<span class="prequizTitle">WELCOME TO THE PREQUIZ!</span>
-							<p>This quiz is to help re-evaluate what you have already learned before you move onto the next chapter.</p>
-							<br>
-							<button class="readyButton col-md-3 col-xs-12">I'm Ready</button>
-						</section>
-					</section>
+				<section class="col-xs-12 text-body pretestQuestionsContainer">
+					<p>Does the following graph represent a function?</p>
+					<img class="graphQuestion" src="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>img/math-1050/problem_assets/1.png"/>
+					<br>
+					<input type="radio" id="radio1" name="radios" value="yes" checked>
+				       <label for="radio1">Yes</label>
+				    <input type="radio" id="radio2" name="radios" value="no">
+				       <label for="radio2">No</label>
+				</section>
+				<section class="col-md-6 col-md-offset-3 text-center">
+					<button class="readyButton">Next</button>
 				</section>
 			</section>
 		</section>
 	</section>
 </div>
-<footer class="site-footer">
-  <section class="col-xs-4"><img src="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>img/global/left-arrow.png" style="margin-right: 7px;"> BACK</section>
-  <section class="col-xs-8 text-right" style="padding-right:45px;">SKIP PREQUIZ <img src="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>img/global/right-arrow.png"  style="margin-left: 7px;"></section>
+<footer class="site-footer row">
+  	<section class="col-md-4"><img src="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>img/global/left-arrow.png" style="margin-right: 7px;"> BACK</section>
+
+	<section class="col-md-4 text-center">
+		<div class="meterwrapper">
+			<!-- Start of meter -->
+			<div class="meter">
+				<span style="width:10%" title="10%"></span>
+			</div><!-- End of meter -->
+		</div>
+	</section>
+
+ 	<section class="col-md-4 text-right" style="padding-right:45px;">SKIP PREQUIZ <img src="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>img/global/right-arrow.png"  style="margin-left: 7px;"></section>
 
 </footer>
 
