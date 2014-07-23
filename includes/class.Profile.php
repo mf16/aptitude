5ed4bac3db05e473c44c5af56534ae39171483fe
Tyler Slater <tslater@goaptitude.com>
2014-07-22 22:40:30 -0400

    Merging our files on hack night

--- includes/class.Profile.php
+++ includes/class.Profile.php
@@ -21,6 +21,82 @@ class Profile {
 	function draw(){
 		drawHeader($this->head());
 		?>
+		<style type="text/css">
+		/*New designs css - start*/
+			h1{
+				font-size: 60px;
+				font-family: minion pro;
+				color:#F79234;
+				text-transform:uppercase;
+				letter-spacing:2px;
+			}
+			h3{
+				font-size: 20px;
+				font-family: nexabold;
+				color: #F79234;
+				text-transform: uppercase;
+				letter-spacing: 4px;
+			}
+			.chapter-number>img{
+				width: 100%;
+			}
+			.text-body{
+				padding-left: 0px !important;
+				margin-top:5px;
+			}
+			.no-padding{
+				padding:0px;
+			}
+			.material-body{
+				padding-left: 0px;
+				padding-top: 8%;
+				padding-right: 25px;
+			}
+			.section-number{
+				font-size: 20px;
+				font-family: nexabold;
+				color: #27292B;
+				text-transform: uppercase;
+				letter-spacing:4px;
+				padding-bottom:30px;
+			}
+			.section-title{
+				margin-bottom: 0px;
+			}
+			.section-title-container{
+				padding-bottom: 30px;
+			}
+			.practiceTitle{
+				font-family: nexabold;
+				color: #F79234;
+				font-size: 21px;
+				padding-top: 30px;
+				letter-spacing: 3px;
+				padding-bottom: 10px;
+			}
+			.practiceWrapper{
+				padding-right: 25px;
+			}
+			.practiceContainer{
+				padding-left: 0px;
+				margin-top:0px;
+			}
+			.practiceNumber{
+				text-align: right;
+				padding-right: 40px;
+			}
+			/*New designs css - end*/
+			.aboutTitle{
+				font-family: open sans;
+				font-weight: 700;
+				letter-spacing: 2px;
+			}
+			span{
+				font-family: open sans;
+				letter-spacing: 2px;
+			}
+		</style>
+		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,700' rel='stylesheet' type='text/css'>
 		<!-- wrapper -->
 		<div class="page-wrap">
 			<div id="slidingMenu">
@@ -50,19 +126,50 @@ class Profile {
 					</article>
 				</div>
 			</header>
-			<section id="headerSpacer"></section>
+			<section id="headerSpacer" style="height: 75px;"></section>
 			<section class="container loader"></section>
+
+			<section class="col-md-2 no-padding">
+				<div class="chapter-number">
+					<img src="<?php echo $_SERVER['DOCUMENT_ROOT'] ?>img/global/solid-arrow.png"/>
+				</div>
+			</section>
+			<section class="col-md-10 material-body">
+				<section class="row-fluid section-title-container">
+					<h1 class="section-title">student profile</h1>
+					<span class="section-number">math 1010-a</span><br>
+				</section>
+				<section class="row">
+					<section class="col-md-3">
+						<img class="roundedProfile" src="<?php echo $_SERVER['DOCUMENT_ROOT'];?>img/global/profile-photo.png">
+						<h3 class="text-center section-number">Ricky Sapp</h3>
+						<section class="col-md-12 dataContainer" style="border: 2px solid #F79234;">
+							<h3>About</h3><br>
+							<p><span class="aboutTitle">Major:</span><span> Economics</span></p>
+							<p><span class="aboutTitle">Year:</span> <span> Sophomore</span></p>
+							<p><span class="aboutTitle">ID #:</span> <span> 26-248-4058</span></p>
+							<br>
+							<h3>Contact</h3><br>
+							<p><span class="aboutTitle">Phone:</span><span> 328-485-4832</span></p>
+							<p><span class="aboutTitle">Email:</span> <span> rickysapp28@gmail.com</span></p>
+							<p><span class="aboutTitle">Address:</span> <br><span> 593 N 183 E<br>Provo, UT 84606</span></p>
+						</section>
+					</section>
+					<section class="col-md-9">
+					</section>
+				</section>
+			</section>
+
+
 			<section class="container body">
 				<section class="row-fluid profile_body">
 					<div class="col-md-3">
 						<section class="row-fluid">
 							<div class="col-md-12">
-								<h3 class="text-center">John Hancock</h3>
-								<img class="roundedProfile" src="<?php echo $_SERVER['DOCUMENT_ROOT'];?>img/global/profile_photo.png">
 							</div>
 						</section>
 						<section class="row-fluid">
-							<section class="col-md-12 dataContainer margin-top">
+							<section class="col-md-12 dataContainer margin-top" style="border: 2px solid #F79234;">
 								<h3>About</h3>
 								<hr class="noMarginTop">
 								<div>
@@ -100,14 +207,23 @@ class Profile {
 					</div>
 					<div class="col-md-9">
 						<section class="row-fluid">
-							<article class="col-lg-12 hidden-xs hidden-sm hidden-md">
+							<article class="col-lg-12 hidden-xs hidden-sm">
+								<table clas="activityTable">
+									<thead>
+									</thead>
+									<tbody>
+
+									</tbody>
+									<tfoot>
+									</tfoot>
+								</table>
 								<section class="activityChart">
 									<?php
 									$days = 0;
 									while ($days < 7){
 										$days++;
 										$weeks = 0;
-										while ($weeks < 49){
+										while ($weeks < 28){
 											$weeks++;
 											$rand = rand(1,4);
 											switch ($rand) {
@@ -129,12 +245,12 @@ class Profile {
 							</article>
 						</section>
 						<section class="row-fluid margin-top">
-							<article class="col-md-12 dataContainer margin-top" style="padding-top:15px; padding-bottom:15px;">
+							<article class="col-md-12 margin-top" style=" padding-top:15px; padding-bottom:15px;">
 								<article id="profileHeroChart" style="height: 500px;"></article>
 							</article>
 						</section>
 						<section class="row-fluid margin-top">
-							<article class="col-md-12 dataContainer" style="padding-top:15px; padding-bottom:15px;">
+							<article class="col-md-12" style="padding-top:15px; padding-bottom:15px;">
 							</article>
 						</section>
 					</div>
